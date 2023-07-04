569574f6fc6d8649386942736a8ca8b890313c28
Rafael Pontes <90840183+rpontestl@users.noreply.github.com>
2023-07-04 11:17:01 -0300

    add glossary

--- parser_params.py
+++ parser_params.py
@@ -8,7 +8,9 @@ To run your experiment with IntPy use:\n\
 $ python "+str(sys.argv[0])+" program_arguments [-h] [-v version, --version version] [--no-cache]\n\n\
 To run in the IntPy DEBUG mode use:\n\
 $ DEBUG=True python "+str(sys.argv[0])+" program_arguments [-h] [-v version, --version version] [--no-cache]\n\n\
-Glossary of the versions:\n\
+"
+def glossary_msg():
+    return "Glossary of the versions:\n\
     =>v01x             : original version with some bug fixes and instrumentation, all data are stored directly in the database.\n\
     =>1d-ow    or v021x: one dicionary (1d), only write (ow), 1st implementation of dictionary: new data is added to the dictionary only when cache miss occur and the function decorated with @deterministic is executed.\n\
     =>1d-ad    or v022x: one dicionary (1d), all data loaded at the begining (ad), 2nd implementation of dictionary (uses 1 dictionary): at the begining of the execution all the data cached is loaded to the dictonary before the user script starts to run.\n\
@@ -17,7 +19,6 @@ Glossary of the versions:\n\
     =>2d-ad-f  or v025x: two dicionaries (2d), all data loaded at the begining of a function(ad-f), 5th implementation of dictionary (uses 2 dictionaries): when @deterministic is executed a select query is created to the database to bring all results of the function decorated with @deterministic stored in the cache. A list of functions already inserted to the dictionary is maintained to avoid unecessary querys to the database. The results are then stored in the dictionary DATA_DICTIONARY. When cache miss occurs and a function decorated with @deterministic is processed, its result is stored in NEW_DATA_DICTIONARY. This way, only the elements of NEW_DATA_DICTIONARY are added to the database at the end of the execution.\n\
     =>2d-ad-ft or v026x: two dicionaries (2d), all data loaded at the begining of a function with a thread (ad-ft), 6th implementation of dictionary (uses 2 dictionaries): when @deterministic is executed a select query is created to the database to bring all results of the function decorated with @deterministic stored in the cache. A list of functions already inserted to the dictionary is maintained to avoid unecessary querys to the database. The results of the query are stored in the dictionary DATA_DICTIONARY by a thread. When cache miss occurs and a function decorated with @deterministic is processed, its result is stored in NEW_DATA_DICTIONARY. This way, only the elements of NEW_DATA_DICTIONARY are added to the database at the end of the execution.\n\
     =>2d-lz    or v027x: two dicionaries (2d), lazy mode (lz), 7th implementation of dictionary (uses 2 dictionaries): new data is added to DATA_DICTIONARY when cache hit occurs (LAZY approach) and new data is added to NEW_DATA_DICTIONARY when cache miss occur and the function decorated with @deterministic is executed."
-
 
 def get_params():
     versions = ['v01x', '1d-ow', 'v021x', '1d-ad', 'v022x', '2d-ad', 'v023x', '2d-ad-t', 'v024x', '2d-ad-f', 'v025x', '2d-ad-ft', 'v026x', '2d-lz', 'v027x']
@@ -36,14 +37,24 @@ def get_params():
                                    metavar='',
                                    nargs=1,
                                    type=str, 
-                                   help='IntPy\'s mechanism version: choose one of the following options: '+', '.join(versions))
-
-    intpy_arg_parser.add_argument('--no-cache',
+                                   help='SpeedUpy\'s mechanism version: choose one of the following options: '+', '.join(versions))
+    
+    intpy_arg_parser.add_argument('-g',
+                                  '--glossary',
+                                  default=False,
+                                  action='store_true',
+                                  help='show details of SpeedUpy versions')
+   
+    intpy_arg_parser.add_argument('-0',
+                                  '--no-cache',
                                   default=False,
                                   action="store_true",
-                                  help='IntPy\'s disable cache')
+                                  help='SpeedUpy\'s disable cache')
 
     args = intpy_arg_parser.parse_args()
+
+    if args.glossary:
+        print(glossary_msg())
 
     argsp_v = args.version
 
